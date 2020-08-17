9735f8f895b4cc397c6cb34c474397af1e76ea70
teitumx <kirik241@ya.ru>
2020-08-17 14:08:04 +0300

    Homework #4

---
+++ lesson-4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <style>
+     .img{
+         width: 400px;
+         height: 200px;
+     }
+ </style>
+    <meta charset="UTF-8">
+    <title>Homework #4</title>
+    <?php
+    include 'main.php';
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
